2b40ca8dc6aeca73ca676edec6cd96d10f64c05c
Eric <rovx15@gmail.com>
2017-01-07 14:11:49 +0100

    Muestra tablero y permite modificar. Todo OK. Comentado.

--- src/model/Tauler.java
+++ src/model/Tauler.java
@@ -5,41 +5,116 @@
  */
 package model;
 
+import java.awt.Color;
+import java.util.Scanner;
+
 /**
  *
  * @author Eric
  */
-public class Tauler {
-    
-    private int files, columnes;
+public final class Tauler {
+
     private Casella[][] tauler;
+    Scanner lector = new Scanner(System.in);
 
+    /**
+     * Constructor de Tauler on li arriba per parametre el numero de files i columnes que tindra el tauler.
+     * S'inicialitza el tauler amb els parametres i es crida al metode completarTauler
+     * @param files
+     * @param columnes 
+     */
     public Tauler(int files, int columnes) {
-        this.files = files;
-        this.columnes = columnes;
         tauler = new Casella[files][columnes];
+        completarTauler();
+    }
+    /**
+     * Aquest metode basicament lo que fa es a cada posicio del tauler introduir un objecte Casella.
+     */
+    public void completarTauler() {
+        for (int i = 0; i < tauler.length; i++) {
+            for (int j = 0; j < tauler.length; j++) {
+                tauler[i][j] = new Casella();
+            }
+        }
     }
 
-    public void setFiles(int files) {
-        this.files = files;
-    }
 
-    public void setColumnes(int columnes) {
-        this.columnes = columnes;
+    /**
+     * Aquest metode demana una fila i una columna per modificar-li l'estat i el color.
+     */
+    public void modificarCasella() {
+        int fila, columna;
+        System.out.print("Introdueix la fila on vols modificar: ");
+        fila = lector.nextInt();
+        System.out.print("Introdueix la columna on vols modificar: ");
+        columna = lector.nextInt();
+        tauler[fila-1][columna-1].setEstat(!tauler[fila][columna].isEstat());
+        tauler[fila-1][columna-1].setColor(Color.RED);
     }
-    
-    
-    
+    /**
+     * Aquest metode mostra el color del tauler.
+     */
+    public void mostrarTauler() {
+        for (int i = 0; i < tauler.length; i++) {
+            for (int j = 0; j < tauler.length; j++) {
+                System.out.print(tauler[i][j].getColor() + " ");
+            }
+            System.out.println("");
+        }
+    }
+    /**
+     * Aquest metode mostra l'estat del tauler.
+     */
+    public void mostrarEstatTauler() {
+        for (int i = 0; i < tauler.length; i++) {
+            for (int j = 0; j < tauler.length; j++) {
+                System.out.print(tauler[i][j].isEstat() + " ");
+            }
+            System.out.println("");
+        }
+    }
+    /**
+     * Aquest metode mostra un menu per escogir si volem veure el tauler o modificar una casella.
+     */
+    public void mostraMenu() {
+        int opcio;
+        do {
+        System.out.print("Escogeix una opcio\n"
+                + "1- Mostra tauler\n"
+                + "2- Modifica casella\n\n"
+                + "3- Sortir\n"
+                + "Opcio: ");
+        
+            opcio = lector.nextInt();
+            switch (opcio) {
+                case 1:
+                    mostrarTauler();
+                    System.out.println("");
+                    mostrarEstatTauler();
+                    break;
+                case 2:
+                    modificarCasella();
+                    break;
+                case 3:
+                    System.exit(0);
+                default:
+                    System.out.println("Selecciona una opcio valida!");
+                    break;
+            }
+        } while (true);
+    }
 }
 
-class Casella{
-    
-    private boolean estat;
-    private String color;
+class Casella {
 
-    public Casella(boolean estat, String color) {
-        this.estat = estat;
-        this.color = color;
+    private boolean estat;
+    private Color color;
+    /**
+     * Constructor de casella on s'inicialitzan totes per defecte en false y GREEN
+     */
+    public Casella() {
+        this.estat = false;
+        this.color = Color.GREEN;
     }
 
     public boolean isEstat() {
@@ -50,14 +125,12 @@ class Casella{
         this.estat = estat;
     }
 
-    public String getColor() {
+    public Color getColor() {
         return color;
     }
 
-    public void setColor(String color) {
+    public void setColor(Color color) {
         this.color = color;
     }
-    
-    
-    
+
 }
--- src/practica4u2/MainClass.java
+++ src/practica4u2/MainClass.java
@@ -1,14 +1,12 @@
 package practica4u2;
 
-import control.TaulerControl;
-import java.util.Scanner;
 import model.Tauler;
 
 
 public class MainClass {
     
     public static void main(String[]args){
-        TaulerControl tc = new TaulerControl();
-        tc.crearTauler();
+        Tauler tau = new Tauler(4, 4);
+        tau.mostraMenu();                
     }
 }
